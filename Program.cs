c35a4a47ca929c42e0a145b9c7505f6f343a1322
redfoxbg <redfoxbg@gmail.com>
2023-06-17 21:25:11 +0300

    Adding functionality for numbers to letters

--- Program.cs
+++ Program.cs
@@ -6,23 +6,43 @@
         {
             int first = 123;
             int second = 222;
-            int rowLength = first.ToString().Length + 3 + second.ToString().Length;
-            int result = first * second;
+            int rowLength = first.ToString().Length + 3 + second.ToString().Length; //Максимална дължина на целия израз
+            int result = first * second; //Резултатът на сметката
 
-            int[] firstMassiv = first.ToString().Select(n => int.Parse(n.ToString())).Reverse().ToArray(); //Convert first parameter to int array
-            //int[] secondMassiv = second.ToString().Select(n => int.Parse(n.ToString())).Reverse().ToArray();//Convert first parameter to int array
+            int[] firstMassive = first.ToString().Select(n => int.Parse(n.ToString())).Reverse().ToArray(); //Преобразуване на първия елемент в масив
+            //int[] secondMassive = second.ToString().Select(n => int.Parse(n.ToString())).Reverse().ToArray();
                    
           
             Console.WriteLine($"{first} * {second}");
             Console.WriteLine(new string('-', rowLength));
 
-            for (int i = 0; i < firstMassiv.Length; i++)
+            for (int i = 0; i < firstMassive.Length; i++)
             {
-                Console.WriteLine($"{new string(' ', second.ToString().Length + 3 - i)}{firstMassiv[i] * second}");
+                Console.WriteLine($"{new string(' ', second.ToString().Length + 3 - i)}{firstMassive[i] * second}");
             }
 
             Console.WriteLine(new string('-', rowLength));
-            Console.WriteLine($"{new string(' ', )}");
+            Console.WriteLine($"{new string(' ', rowLength - result.ToString().Length)}{result}");
+
+
+
+            Random random = new Random();
+           
+            List<char> letters = Enumerable.Range('A', 26).Select(x => (char)x).ToList(); // Създаване на списък с буквите от английската азбука
+           
+            letters = letters.OrderBy(x => random.Next()).Take(10).ToList();  // Произволно разбъркване на списъка с буквите и избиране на първите 10
+
+            Dictionary<char, int> letterToDigit = new Dictionary<char, int>(); // Заместване на буквите с цифрите от 0 до 9
+            for (int i = 0; i < letters.Count; i++)
+            {
+                letterToDigit[letters[i]] = i;
+            }
+            
+            foreach (var kvp in letterToDigit) // Отпечатване на съответствието между буквите и цифрите
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+
 
 
         }
