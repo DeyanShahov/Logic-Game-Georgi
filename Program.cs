aff6d5e268457bb897d973f66a082f4bb8e8ec9b
redfoxbg <redfoxbg@gmail.com>
2023-06-17 23:45:57 +0300

    Fixing errors

--- Program.cs
+++ Program.cs
@@ -10,21 +10,21 @@ namespace Logic_Game_Georgi
     {
         static void Main(string[] args)
         {
-            int first = 123;
-            int second = 222;
+            long first = 1234;
+            long second = 22213221;
             int rowLength = first.ToString().Length + 3 + second.ToString().Length; // Максимална дължина на целия израз
-            int result = first * second; // Резултатът на сметката
+            long result = first * second; // Резултатът на сметката
 
             int[] firstMassive = first.ToString().Select(n => int.Parse(n.ToString())).Reverse().ToArray(); // Преобразуване на първия елемент в масив
-            //int[] secondMassive = second.ToString().Select(n => int.Parse(n.ToString())).Reverse().ToArray();
+            int[] secondMassive = second.ToString().Select(n => int.Parse(n.ToString())).Reverse().ToArray();
 
 
             Console.WriteLine($"{first} * {second}");
             Console.WriteLine(new string('-', rowLength));
 
-            for (int i = 0; i < firstMassive.Length; i++)
+            for (int i = 0; i < secondMassive.Length; i++)
             {
-                Console.WriteLine($"{new string(' ', second.ToString().Length + 3 - i)}{firstMassive[i] * second}");
+                Console.WriteLine($"{new string(' ', second.ToString().Length + 3 - i)}{first * secondMassive[i]}");
             }
 
             Console.WriteLine(new string('-', rowLength));
@@ -69,18 +69,18 @@ namespace Logic_Game_Georgi
 
             toPrint = string.Empty;
 
-            for (int i = 0; i < firstMassive.Length; i++)
+            for (int i = 0; i < secondMassive.Length; i++)
             {
-                Console.WriteLine($"{new string(' ', second.ToString().Length + 3 - i)}{ConvertDigitsToLetter(firstMassive[i] * second, letterToDigit, toPrint)}");
+                Console.WriteLine($"{new string(' ', second.ToString().Length + 3 - i)}{ConvertDigitsToLetter(first * secondMassive[i], letterToDigit, toPrint)}");
             }
 
             Console.WriteLine(new string('-', rowLength));
             Console.WriteLine($"{new string(' ', rowLength - result.ToString().Length)}{ConvertDigitsToLetter(result, letterToDigit, toPrint)}");
         }
 
-        private static string ConvertDigitsToLetter(int second, Dictionary<int, char> letterToDigit, string toPrint)
+        private static string ConvertDigitsToLetter(long number, Dictionary<int, char> letterToDigit, string toPrint)
         {
-            foreach (var n in second.ToString())
+            foreach (var n in number.ToString())
             {
                 toPrint += letterToDigit[int.Parse(n.ToString())];
             }
